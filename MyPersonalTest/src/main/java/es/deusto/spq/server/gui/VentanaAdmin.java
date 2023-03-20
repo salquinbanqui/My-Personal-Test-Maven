766e9db58a6ce7b8a6e2ade69e4f6ab502bd179c
josejaviergarcia <72814002+josejaviergarcia@users.noreply.github.com>
2023-03-20 11:27:06 +0100

    hacer las tareas 123

---
+++ MyPersonalTest/src/main/java/es/deusto/spq/server/gui/VentanaAdmin.java
@@ -0,0 +1,10 @@
+package es.deusto.spq.server.gui;
+
+public class VentanaAdmin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MyPersonalTest/src/test/java/es/deusto/spq/server/dataBaseTest/BDTest.java
@@ -0,0 +1,5 @@
+package es.deusto.spq.server.dataBaseTest;
+
+public class BDTest {
+
+}
