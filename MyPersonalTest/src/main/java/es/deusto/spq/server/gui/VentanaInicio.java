8c3ccf69c4468977a502ac4fb6717afec849a2a1
ignacioechavarren <ignacioechavarren@opendeusto.es>
2023-03-20 11:34:48 +0100

    Prueba

--- MyPersonalTest/src/main/java/es/deusto/spq/server/gui/VentanaInicio.java
+++ MyPersonalTest/src/main/java/es/deusto/spq/server/gui/VentanaInicio.java
@@ -3,7 +3,7 @@ package es.deusto.spq.server.gui;
 public class VentanaInicio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hola");
 
 	}
 
