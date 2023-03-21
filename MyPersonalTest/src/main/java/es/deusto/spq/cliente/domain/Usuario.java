a48b3a25886b0ed012df5e9a4728d30e22ab72fb
ignacioechavarren <ignacioechavarren@opendeusto.es>
2023-03-21 08:07:51 +0100

    Clase básica usuario

--- MyPersonalTest/src/main/java/es/deusto/spq/cliente/domain/Usuario.java
+++ MyPersonalTest/src/main/java/es/deusto/spq/cliente/domain/Usuario.java
@@ -1,5 +1,76 @@
 package es.deusto.spq.cliente.domain;
 
 public class Usuario {
+	private String id;
+	private String nombreUsuario;
+	private String password;
+	private String email;
+	private String tarjeta;
+	
+	
+	public Usuario(String nombreUsuario, String password, String email, String tarjeta) {
+		super();
+		this.nombreUsuario = nombreUsuario;
+		this.password = password;
+		this.email = email;
+		this.tarjeta = tarjeta;
+	}
+	
+	public Usuario() {
+		super();
+		this.nombreUsuario = "";
+		this.password = "";
+		this.email = "";
+		this.tarjeta = "";
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTarjeta() {
+		return tarjeta;
+	}
+
+	public void setTarjeta(String tarjeta) {
+		this.tarjeta = tarjeta;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombreUsuario=" + nombreUsuario + ", password=" + password + ", email=" + email
+				+ ", tarjeta=" + tarjeta + "]";
+	}
+	
+	
+	
+	
 }
--- MyPersonalTest/src/main/java/es/deusto/spq/server/gui/VentanaInicio.java
+++ MyPersonalTest/src/main/java/es/deusto/spq/server/gui/VentanaInicio.java
@@ -3,7 +3,7 @@ package es.deusto.spq.server.gui;
 public class VentanaInicio {
 
 	public static void main(String[] args) {
-		System.out.println("hola");
+		
 
 	}
 
