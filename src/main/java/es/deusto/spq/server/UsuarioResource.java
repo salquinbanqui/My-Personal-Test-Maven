9cb48b6e29d4acf9e566d93f13f49e23344862ec
samuelmartin00 <samuel.martin@opendeusto.es>
2023-03-27 16:01:41 +0200

    Update UsuarioResource.java
    añadir/eliminar usuario

--- src/main/java/es/deusto/spq/server/UsuarioResource.java
+++ src/main/java/es/deusto/spq/server/UsuarioResource.java
@@ -1,15 +1,21 @@
 package es.deusto.spq.server;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import java.util.List;
 import java.util.ArrayList;
 import es.deusto.spq.pojo.Usuario;
 
 
-@Path("usuarios")
+@Path("/usuarios")
 public class UsuarioResource {
 	
  @GET
@@ -25,6 +31,23 @@ public class UsuarioResource {
 
 
  return usuarios;
+ }
+ 
+ @POST
+ @Consumes(MediaType.APPLICATION_JSON)
+ public void addUsuario(Usuario usuario) {
+     System.out.println("Received new user: " + usuario);
+ }
+
+ @DELETE
+ @Path("/{code}")
+ public Response deleteUser(@PathParam("code") int code) {
+     if (code == 10) {
+         System.out.println("Deleting user...");
+         return Response.status(Response.Status.OK).build();
+     } else {
+         return Response.status(Response.Status.NOT_FOUND).build();
+     }
  }
 }
 
