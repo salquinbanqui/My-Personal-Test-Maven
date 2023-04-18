e080943c2f42c3e038dc3f63265c913bab6f45de
josejaviergarcia <josejavier.garcia@opendeusto.es>
2023-04-18 18:53:10 +0200

    Junit
    proar junit de usuario

--- src/test/java/es/deusto/spq/cliente/UsuarioTest.java
+++ src/test/java/es/deusto/spq/cliente/UsuarioTest.java
@@ -1,5 +1,41 @@
 package es.deusto.spq.cliente;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.spq.pojo.Usuario;
+
 public class UsuarioTest {
 
+	private Usuario u;
+	
+	@Before
+    public void crearusuario() {
+        u = new Usuario("jose","jose@gmail.com","1234","n_tarjeta",false);
+    }
+	
+	@Test
+	public void testGetNombreUsuario() {
+		assertEquals("jose", u.getNombreUsuario());
+	}
+	
+	@Test
+	public void testSetNombreUsuario() {
+		u.setNombreUsuario("samu");
+		assertEquals("samu", u.getNombreUsuario());
+	}
+	
+	@Test
+	public void testGetPassword() {
+		assertEquals("1234", u.getPassword());
+	}
+	
+	@Test
+	public void testSetPassword() {
+		u.setPassword("4321");
+		assertEquals("4321", u.getPassword());
+	}
+	
 }
