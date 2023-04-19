e52ba05dc35c145c1c66ba57c209839a0991a7bf
ignacioechavarren <ignacioechavarren@opendeusto.es>
2023-04-19 10:57:46 +0200

    Clase Pelicula y PeliculaTest hechas

--- src/main/java/es/deusto/spq/pojo/Pelicula.java
+++ src/main/java/es/deusto/spq/pojo/Pelicula.java
@@ -2,4 +2,105 @@ package es.deusto.spq.pojo;
 
 public class Pelicula {
 
+	private String nombre;
+	private String categoria;
+	private double precio;
+	private String fecha;
+	private String descripcion;
+	
+	
+	public Pelicula(String nombre, String categoria, double precio, String fecha, String descripcion) {
+		super();
+		this.nombre = nombre;
+		this.categoria = categoria;
+		this.precio = precio;
+		this.fecha = fecha;
+		this.descripcion = descripcion;
+	}
+	
+	
+	
+	public Pelicula() {
+		super();
+		this.nombre = "";
+		this.categoria = "";
+		this.precio = 0;
+		this.fecha = "";
+		this.descripcion = "";
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+
+
+	public double getPrecio() {
+		return precio;
+	}
+
+
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+
+
+	public String getFecha() {
+		return fecha;
+	}
+
+
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Pelicula [nombre=" + nombre + ", categoria=" + categoria + ", precio=" + precio + ", fecha=" + fecha
+				+ ", descripcion=" + descripcion + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- src/test/java/es/deusto/spq/cliente/PeliculasTest.java
+++ src/test/java/es/deusto/spq/cliente/PeliculasTest.java
@@ -1,5 +1,41 @@
 package es.deusto.spq.cliente;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.spq.pojo.Pelicula;
+
 public class PeliculasTest {
 
+	private Pelicula p;
+	
+	@Before
+    public void crearusuario() {
+        p = new Pelicula("Titanic","Drama",12,"1997","Pelicula nominada a los Oscar");
+    }
+	
+	@Test
+	public void testGetNombre() {
+		assertEquals("Titanic", p.getNombre());
+	}
+	
+	@Test
+	public void testSetNombre() {
+		p.setNombre("UP");
+		assertEquals("UP", p.getNombre());
+	}
+	
+	@Test
+	public void testGetCategoria() {
+		assertEquals("Drama", p.getCategoria());
+	}
+	
+	@Test
+	public void testSetCategoria() {
+		p.setCategoria("Diversion");
+		assertEquals("Diversion", p.getCategoria());
+	}
+	
 }
