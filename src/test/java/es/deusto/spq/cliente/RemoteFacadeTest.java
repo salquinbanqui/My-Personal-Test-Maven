7a66fbc9309f65147276d5ca5ba21c72c594f451
josejaviergarcia <josejavier.garcia@opendeusto.es>
2023-04-20 11:23:45 +0200

    Junit
    hacer prueba unitaria  de remoteFacade

---
+++ src/test/java/es/deusto/spq/cliente/RemoteFacadeTest.java
@@ -0,0 +1,35 @@
+package es.deusto.spq.cliente;
+
+import static org.junit.Assert.*;
+
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import es.deusto.spq.pojo.Usuario;
+import es.deusto.spq.server.DBManager;
+import es.deusto.spq.server.RemoteFacade;
+
+public class RemoteFacadeTest {
+
+	RemoteFacade rf = new RemoteFacade();
+	
+	Usuario u = new Usuario("jose","jose@gmail.com","1234","nº_tarjeta",true);
+	
+	@Mock
+	DBManager db;
+	
+	@Test
+	public void testloginGestionPelis() {
+		assertNotNull(rf.loginGestionPelis(u));
+	}
+
+	
+	@Test
+	@PerfTest(invocations = 10, threads =2)
+	@Required(max = 1200, average = 250)
+	public void testagregarUsuarioGestionPelis() {
+		assertNotNull(rf.agregarUsuarioGestionPelis(u));
+	}
+}
